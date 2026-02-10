69a0ec23334d42d5a483c1280f564743a5ab9738
Giovane Barnes <64332692+GiovaneBarnes@users.noreply.github.com>
2026-02-09 18:24:08 -0600

    Time: 239 ms (47.61%), Space: 47.3 MB (36.44%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,53 +1,59 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        Trie myTrie = buildTrie(words);
-        Set<String> wordsToBeReturned = new HashSet<>();
+        Trie myTrie = buidTrie(words); 
+        Set<String> unqiueWords = new HashSet<>();
         for(int row = 0; row < board.length; row++){
-            for(int col = 0;  col < board[0].length; col++){
-                dfs(board, myTrie, row, col, wordsToBeReturned);
+            for(int col = 0; col < board[0].length; col++){
+                dfs(board, myTrie, row, col, unqiueWords);
             }
         }
-        return new ArrayList<String>(wordsToBeReturned);
+        return new ArrayList<>(unqiueWords);
     }
 
-    private void dfs(char[][] board, Trie myTrie, int row, int col, Set<String> wordsToBeReturned){
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length){
+    private void dfs(char[][] board, Trie myTrie, int row, int col, Set<String> unqiueWords){
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length){ 
             return;
         }
-        if (board[row][col] == '#'){
-            return; 
-        } 
+
+        char letter = board[row][col];
+        int trieIDX =  letter - 'a';
         
-        char c =  board[row][col];
-        int idx = c - 'a';
-        if (myTrie.next[idx] == null){
+        if (board[row][col] == '#'){ 
+            return; 
+        }
+
+        Trie nextNode = myTrie.next[trieIDX];
+
+        if (nextNode == null){
             return;
         }
 
-        if(myTrie.next[idx].word != null){
-           wordsToBeReturned.add(myTrie.next[idx].word);
+
+        if (nextNode.word != null){
+            unqiueWords.add(nextNode.word);
         }
 
-        myTrie = myTrie.next[idx]; 
-        char toBeReplaced = board[row][col];
         board[row][col] = '#';
-        dfs(board, myTrie, row + 1, col, wordsToBeReturned);
-        dfs(board, myTrie, row - 1, col, wordsToBeReturned);
-        dfs(board, myTrie, row, col + 1, wordsToBeReturned);
-        dfs(board, myTrie, row, col - 1, wordsToBeReturned);
-        board[row][col] = toBeReplaced; 
+        myTrie = myTrie.next[trieIDX];
+
+        dfs(board, myTrie, row + 1, col, unqiueWords);
+        dfs(board, myTrie, row - 1, col, unqiueWords);
+        dfs(board, myTrie, row, col + 1, unqiueWords);
+        dfs(board, myTrie, row, col - 1, unqiueWords);
+
+        board[row][col] = letter;
     }
 
-
-    private Trie buildTrie(String[] words){
-        Trie root = new Trie(); 
+    private Trie buidTrie(String[] words){
+        Trie root = new Trie();
         for(String word: words){
             Trie node = root;
-            for(char letter: word.toCharArray()){
-                if (node.next[letter-'a'] == null){
+            for(char letter: word.toCharArray()) {
+                if(node.next[letter-'a'] == null){
                     node.next[letter-'a'] = new Trie();
                 }
-                node = node.next[letter-'a'];
+
+                node = node.next[letter - 'a'];
             }
             node.word = word;
         }
@@ -55,8 +61,7 @@ class Solution {
     }
 
     private class Trie{
-        Trie[] next = new Trie[26];
-        String word; 
+        Trie[] next = new Trie[26]; 
+        String word;
     }
-    
 }
